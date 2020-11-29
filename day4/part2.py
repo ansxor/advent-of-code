4f0bc2fbf601ea1c7e2c600dd0c64641a3327ecc
Darien <answerxox@gmail.com>
2020-11-28 21:57:55 -0500

    day 4

--- day4/part2.py
+++ day4/part2.py
@@ -4,10 +4,11 @@ count = 0
 
 for i in range(264793, 803936):
     condition = True
-    
+
     double_rx = re.compile(r'(.)\1')
     num = '{:0>6}'.format(i)
     nums = [int(x) for x in num]
+    double_num = 0
     double = False
     not_decreasing = True
     oldnum = 0
@@ -16,8 +17,15 @@ for i in range(264793, 803936):
             not_decreasing = False
             break
         if j == oldnum:
+            double_num += 1
+        elif double_num == 1:
+            double_num = 0
             double = True
+        else:
+            double_num = 0
         oldnum = j
+    if double_num == 1:
+        double = True
     if not_decreasing and double:
         count += 1
 
